0769980fc8c996b8767ef1868c8baa6483d326b8
AkramQureshi <161283057+AkramQureshi@users.noreply.github.com>
2024-03-31 22:25:43 +0500

    Add files via upload

---
+++ myFunction.ts
@@ -0,0 +1,76 @@
+
+
+
+function myfunc()
+{
+    
+    console.log("Hellow Function")
+return 
+}
+myfunc()
+ 
+
+
+/*
+
+function myFunc(){
+    let total = 2+2
+    console.log(total)
+
+}
+myFunc()
+
+*/
+/*
+function myFunc(){
+    let total = 7+3
+    return total
+    }
+    let totalNum = myFunc()
+    console.log("Total Number", totalNum)
+*/
+
+/*function add(){
+   let numb= 3+2
+    return numb
+} 
+    let total = add()
+   console.log(total)
+  
+   */
+
+/*
+function myFunc(){
+    let num1 = 10
+    let num2 = 5
+    let total=(num1+num2)
+return total    
+}
+let total=myFunc()
+console.log(total)
+
+
+/*
+function myFunc(){
+    let total = 2+2
+    return total
+}
+let total1Num= myFunc()
+
+console.log("total1Num",total1Num)  
+
+*/
+
+
+/*
+
+function myfun1(){
+let a=2, b=5, c=8,
+total = a+b+c
+return total
+}
+let total=myfun1()
+console.log(total)
+
+*/
+       
